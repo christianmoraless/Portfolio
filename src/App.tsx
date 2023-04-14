2e4d770fea93b398ab07c2a5bd4442be32963cd3
christianmoraless <christiandmoraless@gmail.com>
2023-04-14 16:53:58 -0400

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from "./components";
+
 function App() {
-  return <div>Portfolio 3D</div>;
+  return (
+    <BrowserRouter>
+      <div className="relativer z-0 bg-primary">
+        <div className="bg-hero-patter bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
