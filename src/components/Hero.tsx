4a9448fb2544889f6263185954ece643cab687b3
christianmoraless <christiandmoraless@gmail.com>
2023-04-15 18:57:27 -0400

    feat:navbar end

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import { motion } from "framer-motion";
+// Styles
+import { styles } from "../styles";
+// Canvas
+import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-autp flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
