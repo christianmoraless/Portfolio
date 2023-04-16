5158a0feb3dbb9e7999708ac0d330eb4c4dfcec7
christianmoraless <christiandmoraless@gmail.com>
2023-04-15 21:19:14 -0400

    feat:creating 3d image section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,14 +5,27 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-autp flex flex-row items-start gap-5`}>
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1
+            className={`${styles.heroHeadText} text-white text-5xl font-bold`}>
+            Hi, I'm <span className="text-[#915eff]">Christian</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop wonderful user interfaces{" "}
+            <br className="sm:block hidden" />
+            and web applications
+          </p>
         </div>
       </div>
+      {/* COMPUTER IMAGE */}
+      <ComputersCanvas />
     </section>
   );
 };
