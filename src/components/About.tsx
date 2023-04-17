191aef4e66e855e3816522031a6d8d8784c079fa
christianmoraless <christiandmoraless@gmail.com>
2023-04-17 15:57:35 -0400

    feat:adding content to about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,8 +3,45 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../util/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{services.title}</p>;
+};
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <motion.div>
+        <p
+          className={`sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider`}
+        >
+          Introduction
+        </p>
+        <h2
+          className={`text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]`}
+        >
+          Overview.
+        </h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
