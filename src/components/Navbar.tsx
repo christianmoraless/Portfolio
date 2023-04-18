e04b88ade45f1799d4cba67e1933932b1f77202a
christianmoraless <christiandmoraless@gmail.com>
2023-04-18 17:03:32 -0400

    feat:works section end

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,8 @@ const Navbar = () => {
   return (
     <div>
       <nav
-        className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+        className={`sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      >
         <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
           <Link
             to="/"
@@ -24,7 +25,8 @@ const Navbar = () => {
             onClick={() => {
               setActive("");
               window.scrollTo(0, 0);
-            }}>
+            }}
+          >
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
             <p className="text-white text-[18px] font-bold curson-pointer">
               Christian <span className="sm:block hidden">| Web developer</span>
@@ -37,7 +39,8 @@ const Navbar = () => {
                 className={`${
                   active === link.title ? "text-white" : "text-secondary"
                 } hover:text-white text-[18px] font-medium cursor-pointer`}
-                onClick={() => setActive(link.title)}>
+                onClick={() => setActive(link.title)}
+              >
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>
             ))}
@@ -53,7 +56,8 @@ const Navbar = () => {
             <div
               className={`${
                 !toggle ? "hidden" : "flex"
-              } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+              } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            >
               <ul className="list-none flex justify-end items-start flex-col gap-4">
                 {navLinks.map((link) => (
                   <li
@@ -64,7 +68,8 @@ const Navbar = () => {
                     onClick={() => {
                       setToggle(!toggle);
                       setActive(link.title);
-                    }}>
+                    }}
+                  >
                     <a href={`#${link.id}`}>{link.title}</a>
                   </li>
                 ))}
